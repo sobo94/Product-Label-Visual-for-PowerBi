4e73de64f5c8b75f045017fda2f32b80c8fd8d2e
Ala Khalil <khalilala1@gmail.com>
2018-08-27 15:48:37 -0400

    Successfully tested application framework!

---
+++ DCWIMS MENU/app.js
@@ -0,0 +1,27 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Hello = /** @class */ (function (_super) {
+    __extends(Hello, _super);
+    function Hello() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Hello.prototype.render = function () {
+        return (React.createElement("h1", null, "Welcome to React!!"));
+    };
+    return Hello;
+}(React.Component));
+ReactDOM.render(React.createElement(Hello, null), document.getElementById('root'));
+//# sourceMappingURL=app.js.map
